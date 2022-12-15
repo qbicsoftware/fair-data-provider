de78f5f4c6dc3cbeda224e4a99edd5b9a74672b3
Sven Fillinger <sven.fillinger@mailbox.org>
2022-12-15 10:42:14 +0100

    Add project structure

---
+++ dataset-pagebuilder/src/main/java/life/qbic/Main.java
@@ -0,0 +1,8 @@
+package life.qbic;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
---
+++ dataset-pagebuilder/src/test/groovy/life/qbic/MainSpec.groovy
@@ -0,0 +1,24 @@
+package life.qbic
+
+import spock.lang.Specification
+
+/**
+ * <b><class short description - 1 Line!></b>
+ *
+ * <p><More detailed description - When to use, what it solves, etc.></p>
+ *
+ * @since <version tag>
+ */
+class MainSpec extends Specification {
+
+    def "see if the spock tests work"() {
+
+        when:
+        def a = 2
+
+        then:
+        a == 2
+
+    }
+
+}
