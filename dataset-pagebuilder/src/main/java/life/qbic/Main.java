9389e7b304092e9f5e952ad21d36fd095c17379d
Aline-9 <aline.breitinger@student.uni-tuebingen.de>
2022-12-21 14:08:55 +0100

    configuration of template engine

--- dataset-pagebuilder/src/main/java/life/qbic/Main.java
+++ dataset-pagebuilder/src/main/java/life/qbic/Main.java
@@ -1,6 +1,9 @@
 package life.qbic;
 
+import freemarker.template.Configuration;
+
 import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 public class Main {
@@ -10,6 +13,14 @@ public class Main {
 
     File file = new File("C://Users/aline/Dokumente/Studium/Semester_7/Bachelorarbeit/fairy-data-provider/dataset-pagebuilder/src/main/resources/jsonld_example.json");
     Map dataModel = JsonReader.readFile(file);
+
+    try {
+      Configuration cfg = TemplateEngine.initiate();
+      TemplateEngine.buildPage(cfg);
+    }catch(IOException e){
+      e.printStackTrace();
+    }
+
 
   }
 
--- dataset-pagebuilder/src/main/java/life/qbic/TemplateEngine.java
+++ dataset-pagebuilder/src/main/java/life/qbic/TemplateEngine.java
@@ -1,5 +1,24 @@
 package life.qbic;
 
-public class TemplateEngine {
+import freemarker.template.*;
+import java.io.File;
+import java.io.IOException;
+
+public  class TemplateEngine {
+
+    public static Configuration initiate() throws IOException {
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_29);
+        cfg.setDirectoryForTemplateLoading(new File("C://Users/aline/Dokumente/Studium/Semester_7/Bachelorarbeit/fairy-data-provider/dataset-pagebuilder/src/main/resources"));
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
+        cfg.setLogTemplateExceptions(false);
+        cfg.setWrapUncheckedExceptions(true);
+        cfg.setFallbackOnNullLoopVariable(false);
+        return(cfg);
+    }
+    public static void buildPage(Configuration cfg){
+        //TODO
+    }
 
 }
+
