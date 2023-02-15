b9855b09c9e0980f633517f40aef567819e1761c
Aline-9 <aline.breitinger@student.uni-tuebingen.de>
2023-02-16 00:58:42 +0100

    Scanning for null values before creating html page

--- dataset-pagebuilder/src/main/java/life/qbic/FileHandler.java
+++ dataset-pagebuilder/src/main/java/life/qbic/FileHandler.java
@@ -52,7 +52,7 @@ public class FileHandler {
         dataModel.put("@context","http://schema.org");
         dataModel.put("@type","dataset");
         for (int i = 0; i < keys.length; i++) {
-            if(nonNull(keys[i]) && nonNull(values[i])) {
+            if(nonNull(keys[i]) && nonNull(values[i]) && !values[i].equals("null")) {
                 dataModel.put(keys[i], values[i]);
             }
         }
